b3d3e3963be0b1fd8ea10911344af5a88ffe46d2
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-11 13:19:22 +0100

    35. Add a single-line comment that says initialize buttons.

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+//initialize buttons
+
 function goStore() {
   return console.log("Going to store.");
 }
