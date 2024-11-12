d3f542472d98303325401f74d0097c27b2d24cee
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 15:49:59 +0100

    151. Add an else statement to the first if statement inside your attack() function. In the else statement, use the += operator to add the text " You miss." to the end of text.innerText.

--- index.js
+++ index.js
@@ -182,6 +182,8 @@ function attack() {
   if (isMonsterHit()) {
     monsterHealth -=
       weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
   }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
