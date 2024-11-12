7fd2fc9304dc4a8083fee115abc78ad76adc55ee
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 14:47:04 +0100

    132. Update goldText and xpText to display the updated values.

--- index.js
+++ index.js
@@ -173,6 +173,8 @@ function dodge() {
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
   xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
 }
 
 function lose() {}
