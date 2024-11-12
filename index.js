343e198478afb304d55d09a1d76ecc403180c3cb
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 11:14:30 +0100

    77. Start by placing all of the code in your buyHealth function inside an if statement. For the if statement condition, check if gold is greater than or equal to 10.

--- index.js
+++ index.js
@@ -73,10 +73,12 @@ function fightDragon() {
 }
 
 function buyHealth() {
-  gold -= 10;
-  health += 10;
-  goldText.innerText = gold;
-  healthText.innerText = health;
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  }
 }
 
 function buyWeapon() {}
