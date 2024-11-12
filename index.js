5d083fc0f8bf5ee48366a1783a919cd618351348
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 13:15:11 +0100

    106. Use the shift() method to take the first element from the inventory array and assign it to your currentWeapon variable.

--- index.js
+++ index.js
@@ -117,6 +117,6 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
-    let currentWeapon;
+    let currentWeapon = inventory.shift();
   }
 }
