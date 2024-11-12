16a78199565c8c688c047f4c80d96d6e9992a7c9
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 11:47:30 +0100

    92. Use the += operator to add text to the end of text.innerText. Add the string " In your inventory you have: " - include the spaces at the beginning and the end.

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeaponIndex].name;
     text.innerText = "You now have a " + newWeapon + ".";
     inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: ";
   }
 }
 
