1836d64fecda6fb4f6ceeed4ed8cbf43e6da56f0
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 10:00:45 +0100

    46. Create an empty update function that takes a parameter called location.

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {
+  console.log(location);
+}
+
 function goTown() {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
