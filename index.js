4ed6f9bb3675cb1a4f9ca5c946c9a2d772c01a36
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 16:14:18 +0100

    164. Inside your while loop, push a random number between 0 and 10 to the end of the numbers array. You can create this random number with Math.floor(Math.random() * 11).

--- index.js
+++ index.js
@@ -259,7 +259,9 @@ function easterEgg() {
 
 function pick(guess) {
   const numbers = [];
-  while (numbers.length < 10) {}
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
 }
 
 function pickTwo() {
