a0f44e98564b5eeaf798ac484c36dc57fca9a068
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-12 15:19:26 +0100

    140. Back to your attack function - inside the else if block, create another if and else statement. If the player is fighting the dragon (fighting would be 2), call the winGame function. Move the defeatMonster() call to the else block.

--- index.js
+++ index.js
@@ -179,7 +179,11 @@ function attack() {
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
+    if (fighting === 2) {
+      winGame();
+    } else {
+      defeatMonster();
+    }
   }
 }
 
